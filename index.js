1ccaddcd8755861d84aca5939829ba2ea761e32a
시혁 박 <exon.indiv@gmail.com>
2020-10-15 19:25:52 +0900

    push

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const nodemailer = require('nodemailer');
+const settings = require('./settings.json')
 
-let transporter = nodemailer.createTransport({
-    // 사용하고자 하는 서비스, gmail계정으로 전송할 예정이기에 'gmail'
-    service: 'gmail',
-    // host를 gmail로 설정
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-      // Gmail 주소 입력, 'testmail@gmail.com'
-      user: process.env.NODEMAILER_USER,
-      // Gmail 패스워드 입력
-      pass: process.env.NODEMAILER_PASS,
-    },
-  });
+const main = async () => {
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: settings.mail_name,
+        pass: settings.mail_password,
+      },
+    });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: `"스학공 알리미" <${settings.mail_name}>`,
+      to: '2020cca005@suwoncca.org',
+      subject: '스스로 학습하자!',
+      text: `7학년 남자 : https://meet.google.com/wmf-nune-dyk
+        7학년 여자 : https://meet.google.com/djh-atun-dgp
+        8학년 남자 : https://meet.google.com/oqh-ggvk-egw
+        8학년 여자 : https://meet.google.com/jik-uevk-hpy
+        9학년 남자 : https://meet.google.com/ixx-gkxz-rci
+        9학년 여자 : https://meet.google.com/sog-ztfg-oha`,
+      html: `<h1>스학공 알리미</h1>
+        <b>오늘도 열심히 공부해보세요!<b><br>
+        7학년 남자 : https://meet.google.com/wmf-nune-dyk<br>
+        7학년 여자 : https://meet.google.com/djh-atun-dgp<br>
+        8학년 남자 : https://meet.google.com/oqh-ggvk-egw<br>
+        8학년 여자 : https://meet.google.com/jik-uevk-hpy<br>
+        9학년 남자 : https://meet.google.com/ixx-gkxz-rci<br>
+        9학년 여자 : https://meet.google.com/sog-ztfg-oha<br>`,
+    });
+  
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  };
+  
+  main().catch(console.error);
