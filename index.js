6a187e8ec4d8e2937e67f2a598b3b9748cddc870
시혁 박 <exon.indiv@gmail.com>
2020-10-15 21:40:11 +0900

    edit settings.json rescue

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const main = async () => {
       port: 587,
       secure: false,
       auth: {
-        user: settings.mail_name,
-        pass: settings.mail_password,
+        user: settings.from.name,
+        pass: settings.from.password,
       },
     });
   
     // send mail with defined transport object
     let info = await transporter.sendMail({
       from: `"스학공 알리미" <${settings.mail_name}>`,
-      to: '2020cca005@suwoncca.org',
+      to: settings.to,
       subject: '스스로 학습하자!',
       text: `7학년 남자 : https://meet.google.com/wmf-nune-dyk
         7학년 여자 : https://meet.google.com/djh-atun-dgp
@@ -34,8 +34,7 @@ const main = async () => {
         9학년 여자 : https://meet.google.com/sog-ztfg-oha<br>`,
     });
   
-    console.log('Message sent: %s', info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log(`Message sent: ${info.messageId}`);
   };
   
   main().catch(console.error);
