a2d1d0fa9a7466ab550cfdd26bbbc707602eea32
시혁 박 <exon.indiv@gmail.com>
2020-10-18 09:08:08 +0900

    Add content

--- index.js
+++ index.js
@@ -20,27 +20,14 @@ const main = async () => {
       from: `"스학공 알리미" <${settings.mail_name}>`,
       to: settings.to,
       subject: '스스로 학습하자!',
-      text: `7학년 남자 : https://meet.google.com/wmf-nune-dyk
-        7학년 여자 : https://meet.google.com/djh-atun-dgp
-        8학년 남자 : https://meet.google.com/oqh-ggvk-egw
-        8학년 여자 : https://meet.google.com/jik-uevk-hpy
-        9학년 남자 : https://meet.google.com/ixx-gkxz-rci
-        9학년 여자 : https://meet.google.com/sog-ztfg-oha`,
-      html: `<h1>스학공 알리미</h1>
-        <b>오늘도 열심히 공부해보세요!<b><br>
-        7학년 남자 : https://meet.google.com/wmf-nune-dyk<br>
-        7학년 여자 : https://meet.google.com/djh-atun-dgp<br>
-        8학년 남자 : https://meet.google.com/oqh-ggvk-egw<br>
-        8학년 여자 : https://meet.google.com/jik-uevk-hpy<br>
-        9학년 남자 : https://meet.google.com/ixx-gkxz-rci<br>
-        9학년 여자 : https://meet.google.com/sog-ztfg-oha<br>`,
+      html: settings.content,
     })
   
     console.log(`Message sent: ${info.messageId}`)
 }
 
-const stop = () => { console.log('[System] Stop') }
+const stop = () => console.log('[System] Stop')
 const task = () => main().catch(console.error)
-
-const job = new CronJab('0 25 7 MON,WED,THU * *', task, stop, true, 'Asia/Seoul')
+task()
+//const job = new CronJab('0 25 7 MON,WED,THU * *', task, stop, true, 'Asia/Seoul')
 
